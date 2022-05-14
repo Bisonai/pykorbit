e6bc22350a49702b2fc39150f602fb1bff3eb4db
Martin Kersner <m.kersner@gmail.com>
2022-05-14 16:03:31 +0900

    feat: Fetch detailed ticker from public REST API

--- pykorbit/rest_public.py
+++ pykorbit/rest_public.py
@@ -7,7 +7,34 @@ from .utils import send_get_request
 class KorbitRestPublic:
     @staticmethod
     def ticker(currency_pair: str) -> Dict[str, Any]:
-        # https://apidocs.korbit.co.kr/#ticker
+        """https://apidocs.korbit.co.kr/#ticker
+
+        Example response:
+            {
+                "timestamp": 1389678052000,
+                "last": "569000"
+            }
+        """
+        url = "https://api.korbit.co.kr/v1/ticker"
+        params = [("currency_pair", currency_pair)]
+        return send_get_request(url, params=params)
+
+    def detailed_ticker(currency_pair: str) -> Dict[str, Any]:
+        """https://apidocs.korbit.co.kr/#detailed-ticker
+        Example response:
+            {
+                "timestamp": 1558590089274,
+                "last": "9198500",
+                "open": "9500000",
+                "bid": "9192500",
+                "ask": "9198000",
+                "low": "9171500",
+                "high": "9599000",
+                "volume": "1539.18571988",
+                "change": "-301500",
+                "changePercent": "-3.17"
+            }
+        """
         url = "https://api.korbit.co.kr/v1/ticker"
         params = [("currency_pair", currency_pair)]
         return send_get_request(url, params=params)
@@ -19,7 +46,10 @@ class KorbitRestPublic:
             KorbitUnexpectedResponse
         """
         url = "https://api.korbit.co.kr/v1/constants"
-        try:
-            return send_get_request(url).get("exchange")
-        except Exception as e:
-            raise KorbitUnexpectedResponse from e
+        response = send_get_request(url)
+        response_echange = response.get("exchange")
+
+        if response_echange:
+            return response_echange
+        else:
+            raise KorbitUnexpectedResponse(response_echange)
