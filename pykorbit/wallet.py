48353527d80ce6376bf6cecfb85aa0b467f8ff2d
Martin Kersner <m.kersner@gmail.com>
2022-05-14 15:33:39 +0900

    chore: Add links to documentation

--- pykorbit/wallet.py
+++ pykorbit/wallet.py
@@ -11,6 +11,13 @@ TRANSFER_TYPES = (
 
 
 class KorbitWallet:
+    """
+    TODO
+    * Request Crypto Currency Withdrawal
+    * Cancel Transfer Request
+    * Assign Crypto Currency Address
+    """
+
     @staticmethod
     def view_transfers(
         access_token: str,
@@ -19,6 +26,7 @@ class KorbitWallet:
         offset: int = 0,
         limit: int = 40,
     ) -> List:
+        # https://apidocs.korbit.co.kr/#view-transfers
         logging.debug("View Transfers")
 
         url = "https://api.korbit.co.kr/v1/user/transfers"
@@ -47,6 +55,7 @@ class KorbitWallet:
         currency: str,
         request_id: Optional[int] = None,
     ) -> List[Dict]:
+        # https://api.korbit.co.kr/v1/user/coins/status
         logging.debug("Query Status of Deposit and Withdrawal")
 
         url = "https://api.korbit.co.kr/v1/user/coins/status"
