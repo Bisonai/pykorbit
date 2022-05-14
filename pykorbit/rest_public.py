10a622b4b259786d850166a99f0280494ac256bb
Martin Kersner <m.kersner@gmail.com>
2022-05-14 15:56:30 +0900

    feat: Add ticker for public REST API

--- pykorbit/rest_public.py
+++ pykorbit/rest_public.py
@@ -1,10 +1,17 @@
-from typing import Dict
+from typing import Any, Dict
 
 from .exception import KorbitUnexpectedResponse
 from .utils import send_get_request
 
 
 class KorbitRestPublic:
+    @staticmethod
+    def ticker(currency_pair: str) -> Dict[str, Any]:
+        # https://apidocs.korbit.co.kr/#ticker
+        url = "https://api.korbit.co.kr/v1/ticker"
+        params = [("currency_pair", currency_pair)]
+        return send_get_request(url, params=params)
+
     @staticmethod
     def constants() -> Dict:
         """
