3c476c7889e93dbb6e60b4aaf6f5c2ba2555d4dd
Martin Kersner <m.kersner@gmail.com>
2022-05-14 16:12:58 +0900

    feat: Fetch list of filled orders through public REST API

--- pykorbit/rest_public.py
+++ pykorbit/rest_public.py
@@ -1,7 +1,13 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 from .exception import KorbitUnexpectedResponse
 from .utils import send_get_request
+
+_ALLOWED_TIMES = (
+    "day",
+    "hour",
+    "minute",
+)
 
 
 class KorbitRestPublic:
@@ -11,8 +17,8 @@ class KorbitRestPublic:
 
         Example response:
             {
-                "timestamp": 1389678052000,
-                "last": "569000"
+              "timestamp": 1389678052000,
+              "last": "569000"
             }
         """
         url = "https://api.korbit.co.kr/v1/ticker"
@@ -24,16 +30,16 @@ class KorbitRestPublic:
 
         Example response:
             {
-                "timestamp": 1558590089274,
-                "last": "9198500",
-                "open": "9500000",
-                "bid": "9192500",
-                "ask": "9198000",
-                "low": "9171500",
-                "high": "9599000",
-                "volume": "1539.18571988",
-                "change": "-301500",
-                "changePercent": "-3.17"
+              "timestamp": 1558590089274,
+              "last": "9198500",
+              "open": "9500000",
+              "bid": "9192500",
+              "ask": "9198000",
+              "low": "9171500",
+              "high": "9599000",
+              "volume": "1539.18571988",
+              "change": "-301500",
+              "changePercent": "-3.17"
             }
         """
         url = "https://api.korbit.co.kr/v1/ticker/detailed"
@@ -46,13 +52,47 @@ class KorbitRestPublic:
 
         Example response:
             {
-                "timestamp" : 1386135077000,
-                "bids" : [["1100000", "0.0103918", "1"], ["1000000", "0.01000000", "1"], ... ],
-                "asks" : [["569000", "0.50000000", "1"], ["568500", "2.00000000", "1"], ... ]
+              "timestamp" : 1386135077000,
+              "bids" : [["1100000", "0.0103918", "1"], ["1000000", "0.01000000", "1"], ... ],
+              "asks" : [["569000", "0.50000000", "1"], ["568500", "2.00000000", "1"], ... ]
             }
         """
         url = "https://api.korbit.co.kr/v1/orderbook"
         params = [("currency_pair", currency_pair)]
+        return send_get_request(url, params=params)
+
+    @staticmethod
+    def list_of_filled_orders(
+        currency_pair: str,
+        time: str = "hour",
+    ) -> List[Dict]:
+        """https://apidocs.korbit.co.kr/#list-of-filled-orders
+
+        Examples response:
+            [
+              {
+                "timestamp": 1389678052000,
+                "tid": "22546",
+                "price": "569000",
+                "amount": "0.01000000",
+                "type": "buy"
+              },
+              {
+               "timestamp": 1389678017000,
+               "tid": "22545",
+               "price": "580000",
+               "amount": "0.01000000",
+               "type": "sell"
+             }
+              ...
+            ]
+        """
+        assert time in _ALLOWED_TIMES
+        url = "https://api.korbit.co.kr/v1/transactions"
+        params = [
+            ("currency_pair", currency_pair),
+            ("time", time),
+        ]
         return send_get_request(url, params=params)
 
     @staticmethod
