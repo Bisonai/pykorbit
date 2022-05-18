a6587ccf8f349da974b147270e8e146e58b16bc4
Martin Kersner <m.kersner@gmail.com>
2022-05-18 14:40:36 +0900

    feat: Remove `access_token` from `KorbitWebsocketPublic`

--- pykorbit/websocket_echo.py
+++ pykorbit/websocket_echo.py
@@ -5,15 +5,8 @@ from .websocket_public import KorbitWebsocketPublic
 
 
 class KorbitWebsocketEcho(KorbitWebsocketPublic):
-    def __init__(
-        self,
-        access_token: Optional[str] = None,
-        logging_level: Optional[str] = None,
-    ):
-        super().__init__(
-            access_token=access_token,
-            logging_level=logging_level,
-        )
+    def __init__(self, logging_level: Optional[str] = None):
+        super().__init__(logging_level=logging_level)
 
     async def worker(self, msg: str) -> None:
         msg = json.loads(msg)
